73a6f7d78c0d954f78f0871284040947f5c6599e
srishakatux <srishakatux@gmail.com>
2015-07-14 17:08:00 -0400

    converts QR image to b&w on canvas and then reads QR

--- static/js/decode.js
+++ static/js/decode.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    
+
     qrcode.callback = revealUrl;
 
 	var imageLoader = document.getElementById('js-qr-input');
@@ -13,25 +13,46 @@ $( document ).ready(function() {
 	function handleImage(e){
 	    var reader = new FileReader();
 	    $('#js-scan-btn').button('loading');
+
 	    reader.onload = function(event){
 	    	var canvas = document.getElementById("qr-canvas");
     		var ctx = canvas.getContext("2d");
 	    	var img = new Image();
-	    	img.onload = function(){
+	    	var convertedImg = new Image();
+
+	    	img.onload = function(){	
+
 	            canvas.width = img.width;
 	            canvas.height = img.height;
-	            ctx.drawImage(img,0,0);
-	            qrcode.decode(event.target.result);
+	            ctx.drawImage(img, 0, 0);
+	            
+	            // Processing pixels and thresholding the image
+	            var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	            var threshold = 100;
+
+	            for (var i = 0; i < imgData.data.length; i += 4) {
+				    var r = imgData.data[i];
+				    var g = imgData.data[i+1];
+				    var b = imgData.data[i+2];
+				    var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= threshold) ? 255 : 0;
+				    imgData.data[i] = imgData.data[i+1] = imgData.data[i+2] = v
+				}
+
+				ctx.putImageData(imgData, 0, 0);
+				convertedImg.src = canvas.toDataURL();
+
+				qrcode.decode(convertedImg.src); 
 	        }
-	        img.src = event.target.result;
+	        
+	        img.src = event.target.result; 
 	    }
+
 	    reader.readAsDataURL(e.target.files[0]); 
 	}
 
-
 	function revealUrl(data) {
 		$('#js-scan-btn').button('reset');
-		if (isUrl(data)==true) {
+		if (isUrl(data) == true) {
 			// window.location.replace(data);
 			alert(data);
 		}
