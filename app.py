7a12cdaa5f8e189f0a80d2a3196f508ab7ab5286
kitsandkats <katherine.mcconachie@gmail.com>
2015-07-14 21:09:31 -0400

    adding app functionality to display mlpubkey and transaction id on the certificate; cleaned up app code

--- app.py
+++ app.py
@@ -3,38 +3,64 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+MLPUBKEY_PATH = 'keys/ml-certs-public-key.asc'
+TXIDMAP_PATH = 'data/transaction_id_mappings.json'
+
+def read_json(path):
+	with open(path) as json_file:
+		data = json.load(json_file)
+	json_file.close()
+	return data 
+
+def read_file(path):
+	with open(path) as f:
+		data = f.read()
+	f.close()
+	return data
+ 
+def get_txid(data,id):
+	return data[id]
+
+def check_display(award):
+	if award['display'] == 'FALSE':
+		award['subtitle'] = '';
+	return award
+
 @app.route('/')
 def home_page():
 	return render_template('index.html')
 
-@app.route('/hierarchy')
-def test():
-	return "trial"
+@app.route('/keys/ml-certs-public-key.asc')
+def mlpubkey_page():
+	content = read_file(MLPUBKEY_PATH)
+	return content
 
 @app.route('/<id>')
 def award(id=None):
 	if id:
+		pubkey_content = read_file(MLPUBKEY_PATH)
+		txidmap_content = read_json(TXIDMAP_PATH)
+		tx_id = get_txid(txidmap_content,id)
 		try:
-			with open('jsons/'+id+'.json') as json_data:
-				recipient = json.load(json_data)
-				json_data.close()
+			recipient = read_json('data/jsons/'+id+'.json')
 		except IOError:
-			return 'Invalid URL'
+			return 'Invalid URL'	
 		if recipient:
 			award = {
 				"logoImg": recipient["certificate"]["issuer"]["image"],
-				"signatureImg": recipient["assertion"]["image:signature"],
 				"name": recipient["recipient"]["givenName"]+' '+recipient["recipient"]["familyName"],
 				"title": recipient["certificate"]["title"],
 				"subtitle": recipient["certificate"]["subtitle"]["content"],
 				"display": recipient["certificate"]["subtitle"]["display"],
 				"organization":recipient["certificate"]["issuer"]["name"],
 				"text": recipient["certificate"]["description"],
-				"mlPublicKey": 'asdkfiawufelajwnflajsdfuhwaefjafasdfasdkfiawufelajwnflajsdfuhwaefjafasdfasdkfiawufelajwnflajsdfuhwaefjafasdfasdkfiawufelajwnflajsdfuhwaefjafasdf',
-				"signature": recipient["signature"]
+				"signatureImg": recipient["assertion"]["image:signature"],
+				"mlPublicKey": pubkey_content,
+				"mlPublicKeyURL": recipient["verify"]["signer"],
+				"transactionID": tx_id,
+				"transactionIDURL": 'https://blockchain.info/tx/'+tx_id
 			}
-			if award['display'] == 'FALSE':
-				award['subtitle'] = '';
+			award = check_display(award)
 			return render_template('award.html', award=award)
 	else:
 		return "Error, please try again."
