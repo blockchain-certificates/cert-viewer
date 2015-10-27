fbbfbdf97f17b83887fb96eb9aeeed8ff9f76978
jnazare <jules.nazare@gmail.com>
2015-10-27 11:43:41 -0400

    adding fields

--- helpers.py
+++ helpers.py
@@ -52,8 +52,9 @@ def updateRequested(user):
 
 def createUser(form):
 	userJson = {}
-	userJson["issued"] = None
+	userJson["issued"] = False
 	userJson["txid"] = None
+	userJson["requested"] = True
 	userJson["user"] = {}
 	userJson["user"]["email"] = form.email.data
 	userJson["user"]["name"] = {"familyName": form.last_name.data, "givenName": form.first_name.data}
