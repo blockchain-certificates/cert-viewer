64fc847eda8b640314b8e32f52612e43c2c47a4a
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-05-23 20:34:29 -0700

    update theme

--- docs/conf.py
+++ docs/conf.py
@@ -126,10 +126,6 @@ todo_include_todos = True
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'alabaster'
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
