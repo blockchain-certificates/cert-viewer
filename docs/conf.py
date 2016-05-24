4e7547744aae957b6ce68c095d9a3b770358801f
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-05-23 18:37:21 -0700

    autodoc include module

--- docs/conf.py
+++ docs/conf.py
@@ -19,8 +19,7 @@ from recommonmark.parser import CommonMarkParser
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.abspath('../conf.ini'))
-#sys.path.append(os.path.abspath('../certificate_viewer'))
+sys.path.append(os.path.abspath('../certificate_viewer'))
 
 # -- General configuration ------------------------------------------------
 
