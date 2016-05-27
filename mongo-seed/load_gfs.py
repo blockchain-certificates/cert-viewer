0689f89fc91fa156fc964e2d6fd8db50d62aa38a
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-05-27 08:20:41 -0700

    update seed data

--- mongo-seed/load_gfs.py
+++ mongo-seed/load_gfs.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     fs = gridfs.GridFS(client.admin)
     with open('68656c6c6f636f6d7077ffff.json') as infile:
         content = infile.read()
-        #fs.put(content, filename='68656c6c6f636f6d7077ffff.json', encoding='utf-8')
-        out = fs.find_one({'filename': '68656c6c6f636f6d7077ffff.json'})
-        print (out.read())
+        fs.put(content, filename='68656c6c6f636f6d7077ffff.json', encoding='utf-8')
+        #out = fs.find_one({'filename': '68656c6c6f636f6d7077ffff.json'})
+        #print (out.read())
 
