60eeac2dd441973152d93b40707fd4cc56dcebe7
Derek Pittsinger <derekpitt@gmail.com>
2016-05-28 10:31:49 -0500

    a little ui

--- viewer/static/js/verification.js
+++ viewer/static/js/verification.js
@@ -1,56 +1,61 @@
-function hidefields(fields){
-	for(i=0; i<fields.length; i++){
-		$(fields[i]).hide();
-	}
-}
+(function() {
 
-function renderResponse(i, len, data){
-	setTimeout(function(){
-		count = i+1;
-		total = len-1;
-		message = data[0];
-		value = data[1];
-		if(i != len-1){
-			$("#progress-msg").html($("#progress-msg").html()+'Step ' + count.toString() +' of ' + total.toString() + "... " +message+'</span>');
-		}
-		setTimeout(function(){
-			if(i == len-1){
-				if(value==true){
-					$("#progress-msg").html($("#progress-msg").html()+"Success! The certificate has been verified.")
-					$("#verified").show();
-				}
-				else{
-					$("#progress-msg").html($("#progress-msg").html()+"Oops! The certificate could not be verified.")
-					$("#not-verified").show();
-				}
-			}
-			else{
-				$("#progress-msg").html($("#progress-msg").html()+'  ['+markMappings[value]+']<br>')
-			}
-		}, 1000)
+  function renderResponse(i, len, data){
+    setTimeout(function() {
+      count = i+1;
+      total = len-1;
+      message = data[0];
+      value = data[1];
 
-	}, timeDelay*i);
-}
+      // not on last step
+      if (i != len-1) {
 
-timeDelay = 2000;
-markMappings = {true: "PASS", false: "FAIL", "DONE": "DONE"}
+        var messageTemplate = $("<li>Step " + count.toString() + " of " + total.toString() + "... " + message + "</li>");
+        $("#progress-msg").append(messageTemplate);
 
-$(document).ready(function() {
-	$( "#verify-button" ).click(function() {
-		hidefields("#not-verified", "#verified");
-		$("#progress-msg").html("");
-		var data = $(this).attr('value');
-		$.get("/verify?"+data, function(res){
-			res = JSON.parse(res);
-			$("#progress-msg").show();
-			if(res == null){
-				$("#progress-msg").html($("#progress-msg").html()+"Oops! There was an issue connecting to the Blockchain.info API")
-			}
-			else{
-				for(i=0; i<res.length; i++){
-					renderResponse(i, res.length, res[i])
-				}
-			}
-		});
-	});
-});
+        setTimeout(function() {
+          messageTemplate.text(messageTemplate.text() + " [" + markMappings[value] + "]");
+        }, 1000);
+
+      } else { // on last one
+        setTimeout(function() {
+          if (value==true) {
+            $("#progress-msg").append("<li>Success! The certificate has been verified.</li>");
+            $("#verified").show();
+          } else {
+            $("#progress-msg").append("<li>Oops! The certificate could not be verified.</li>");
+            $("#not-verified").show();
+          }
+        }, 1000)
+      }
+    }, timeDelay*i);
+  }
+
+  timeDelay = 2000;
+  markMappings = {true: "PASS", false: "FAIL", "DONE": "DONE"}
+
+  $(document).ready(function() {
+    $( "#verify-button" ).click(function() {
+      $("#not-verified, #verified").hide();
+
+      $("#progress-msg").empty();
+
+      var data = $(this).attr('value');
+
+      $.get("/verify?"+data, function(res){
+        res = JSON.parse(res);
+        $("#progress-msg").show();
+
+        if(res == null){
+          $("#progress-msg").append("<li>Oops! There was an issue connecting to the Blockchain.info API</li>");
+        } else {
+          for (i=0; i<res.length; i++) {
+            renderResponse(i, res.length, res[i])
+          }
+        }
+      });
+    });
+  });
+
+
+})();
