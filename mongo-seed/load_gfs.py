d828382ee3675fb15c5c9cbb17b08466ba1d0092
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-10-03 18:11:23 -0700

    update sample data

--- mongo-seed/load_gfs.py
+++ mongo-seed/load_gfs.py
@@ -4,9 +4,10 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient(host='mongodb://localhost:27017')
     fs = gridfs.GridFS(client.test)
-    with open('f813349f-1385-487f-8d89-38a092411fa5.json') as infile:
-        #content = infile.read()
-        #fs.put(content, filename='f813349f-1385-487f-8d89-38a092411fa5.json', encoding='utf-8')
-        out = fs.find_one({'filename': 'f813349f-1385-487f-8d89-38a092411fa5.json'})
-        print(out.read())
-
+    files = ['f813349f-1385-487f-8d89-38a092411fa5.json', 'b5dee02e-50cd-4e48-ad33-de7d2eafa359.json']
+    for f in files:
+        with open(f) as infile:
+            content = infile.read()
+            fs.put(content, filename=f, encoding='utf-8')
+            out = fs.find_one({'filename': f})
+            print(out.read())
