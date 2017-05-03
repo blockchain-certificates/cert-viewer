87430c4f16ea3c83534f3be0d2c1c91cf3d33009
Kim (Hamilton) Duffy <kimdhamilton@gmail.com>
2017-05-02 18:06:09 -0700

    V2 updates (#60)
    * update v2 samples and dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
@@ -7,16 +8,15 @@ here = os.path.abspath(os.path.dirname(__file__))
 install_reqs = parse_requirements('requirements.txt', session=False)
 reqs = [str(ir.req) for ir in install_reqs]
 
-
 with open(os.path.join(here, 'README.md')) as fp:
     long_description = fp.read()
 
 setup(
     name='cert-viewer',
-    version='0.0.2',
+    version='2.0b2',
     url='https://github.com/blockchain-certificates/cert-viewer',
     license='MIT',
-    author='MIT Media Lab Blockchain Certificates',
+    author='Blockcerts',
     author_email='info@blockcerts.org',
     description='A web app for viewing and validating blockchain certificates on the Bitcoin blockchain',
     long_description=long_description,
