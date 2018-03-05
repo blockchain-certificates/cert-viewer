ff811bdf26d70be607034ace23f1784bf20b20d3
xieqihui <xieqihui@gmail.com>
2018-03-05 22:26:15 +0800

    add support for viewing and verifying ethereum certificates

--- tests/test_certificate_formatter.py
+++ tests/test_certificate_formatter.py
@@ -15,6 +15,17 @@ class TestCertificateFormatter(unittest.TestCase):
             self.assertEquals(award['issuedOn'], '2016-09-29')
             self.assertEquals(award['name'], 'Arya Stark')
 
+    def test_certificate_to_award_eth(self):
+        with open('data/1.2/eth-ropsten-cert.json') as cert_file:
+            certificate_json = json.load(cert_file)
+            certificate_model = to_certificate_model(certificate_json)
+            award = certificate_formatter.certificate_to_award(certificate_model)
+            print(award['title'])
+            print(award['issuedOn'])
+            print(award['name'])
+            self.assertEquals(award['title'], 'Certificate of Accomplishment')
+            self.assertEquals(award['issuedOn'], '2017-10-25')
+            self.assertEquals(award['name'], 'Mcallister Greenborough')
 
 
 
